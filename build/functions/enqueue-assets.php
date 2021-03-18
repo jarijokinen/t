bce435bb6cbd79e7845f5b0bf5c785df33baf10d
Jari Jokinen <jari.jokinen@j2.fi>
2021-03-18 16:31:50 +0200

    Remove tap highlighting in Safari/WebKit

--- build/functions/enqueue-assets.php
+++ build/functions/enqueue-assets.php
@@ -3,11 +3,11 @@
 namespace t;
 
 function enqueue_assets() {
-  $css_uri = get_theme_file_uri('css/main.css') . '?1616022003576';
+  $css_uri = get_theme_file_uri('css/main.css') . '?1616077878726';
   wp_enqueue_style(__NAMESPACE__, $css_uri, array(), null);
 
   $js_uri = get_theme_file_uri('js/main.js');
-  wp_enqueue_script(__NAMESPACE__, $js_uri, null, '1616022003576', true);
+  wp_enqueue_script(__NAMESPACE__, $js_uri, null, '1616077878726', true);
 }
 
 function editor_styles() {
@@ -20,7 +20,7 @@ function editor_assets() {
     __NAMESPACE__ . '-editor', 
     get_theme_file_uri('js/editor.js'), 
     null, 
-    '1616022003576', 
+    '1616077878726', 
     true
   );
 }
