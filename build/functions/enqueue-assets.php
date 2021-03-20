577fab2ed140f1aefa80c598575d6c22c9bf8d10
Jari Jokinen <jari.jokinen@j2.fi>
2021-03-20 13:09:41 +0200

    Upgrade packages

--- build/functions/enqueue-assets.php
+++ build/functions/enqueue-assets.php
@@ -3,11 +3,11 @@
 namespace t;
 
 function enqueue_assets() {
-  $css_uri = get_theme_file_uri('css/main.css') . '?1616198567146';
+  $css_uri = get_theme_file_uri('css/main.css') . '?1616238498778';
   wp_enqueue_style(__NAMESPACE__, $css_uri, array(), null);
 
   $js_uri = get_theme_file_uri('js/main.js');
-  wp_enqueue_script(__NAMESPACE__, $js_uri, null, '1616198567146', true);
+  wp_enqueue_script(__NAMESPACE__, $js_uri, null, '1616238498778', true);
 }
 
 function editor_styles() {
@@ -20,7 +20,7 @@ function editor_assets() {
     __NAMESPACE__ . '-editor', 
     get_theme_file_uri('js/editor.js'), 
     null, 
-    '1616198567146', 
+    '1616238498778', 
     true
   );
 }
